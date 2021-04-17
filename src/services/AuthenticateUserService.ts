4c9e3c48e4207670ff0598de1b8305a81919e7f2
fernandes-dev <eduardo.yugan@gmail.com>
2021-04-17 02:45:46 -0400

    added my handler error

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -5,6 +5,8 @@ import { sign } from 'jsonwebtoken';
 import User from '../models/User';
 import authConfig from '../config/auth';
 
+import AppError from '../errors/AppError';
+
 interface Request {
   email: string;
   password: string;
@@ -23,12 +25,12 @@ class AuthenticateUserService {
       where: { email },
     });
 
-    if (!user) throw new Error('Incorrect email/password combination');
+    if (!user) throw new AppError('Incorrect email/password combination', 401);
 
     const passwordMatched = await compare(password, user.password);
 
     if (!passwordMatched)
-      throw new Error('Incorrect email/password combination');
+      throw new AppError('Incorrect email/password combination', 401);
 
     const { secret, expiresIn } = authConfig.jwt;
 
